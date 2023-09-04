346aca655e5d178dae760e796f48297f0a5b8fdf
yuhang zhu <zhucolley@outlook.com>
2023-09-03 20:35:15 -0400

    interactive-rating-component-main done

---
+++ interactive-rating-component-main/app.js
@@ -0,0 +1,71 @@
+// on window load
+window.addEventListener('load', () => {
+  document.body.innerHTML = `<main class='container'>
+      <article>
+        <img src='./images/icon-star.svg' alt='star' />
+      </article>
+      <h1>How did we do?</h1>
+      <p>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <article class='ratings'>
+        <button value='1'>1</button>
+        <button value='2'>2</button>
+        <button value='3'>3</button>
+        <button value='4'>4</button>
+        <button value='5'>5</button>
+      </article>
+      <button type='submit' class='btn'>
+        submit
+      </button>
+    </main>`;
+
+  getAllButtons();
+});
+
+// const buttons = document.querySelector('.ratings').querySelectorAll('buttons');
+function getAllButtons() {
+  const buttons = document.querySelectorAll('.ratings button');
+  //   console.log(buttons);
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      console.log(e.currentTarget.value);
+      document.body.innerHTML = `
+        
+      <main class="container align-center">
+      <article>
+        <img src="./images/illustration-thank-you.svg" alt="star" />
+      </article>
+      <h5 class="rates">You Selected ${e.currentTarget.value} out of 5</h5>
+      <h2>Thank You</h2>
+      <p>
+        we appreciate you taking the time to give a rating, if you ever need
+        more support, don't hesitate to get in touch!
+      </p>
+    </main> 
+      
+      `;
+    });
+  });
+}
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
